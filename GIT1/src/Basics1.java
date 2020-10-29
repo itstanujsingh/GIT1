c7db85a5cb4c52363d253d4aeb544ea4e93f5db5
Tanuj <Tanuj@Tanuj-PC>
2020-10-29 17:41:39 +0530

    This is my Java program commit.

---
+++ GIT1/src/Basics1.java
@@ -0,0 +1,17 @@
+
+public class Basics1 {
+
+	public static void main(String[] args) 
+	{
+		// for printing we use
+				System.out.println("I started Java Learning.");
+				
+				// Now add two numbers and display their sum
+				int i = 5;
+				int j = 3;
+				int k = i+j;
+				System.out.print("Sum of these two numbers is" + k);	
+
+	}
+
+}
